e9c7d19e3cdcc51865415875ef97c413cbdbaa80
AnnanyaV <annanyaved.07@gmail.com>
2020-05-27 19:40:18 +0530

    first commit

---
+++ react-spotify-player/src/Components/Configexample.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "d46070fc432e40a58c7e7cc547a54e7d";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
